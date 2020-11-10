a368067d405dfc5f4a9dddd6ce65fe509d593e58
remy29 <arthur.d.remy@gmail.com>
2020-11-10 03:59:45 +0000

    added .get request to /u/:shortURL in order to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -53,6 +53,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   const rShortURL = generateRandomString();
   urlDatabase[rShortURL] = req.body.longURL;
