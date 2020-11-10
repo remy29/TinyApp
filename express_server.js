bc7ec06b293fa13d5e48c2bfc037aa1842c7aeb2
remy29 <arthur.d.remy@gmail.com>
2020-11-10 04:53:53 +0000

    fixed linting errors

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true})); // allows us to use the body-parser middle-ware to convert request body to readable string
 
-app.set("view engine", "ejs"); // allows us to use ESJ 
+app.set("view engine", "ejs"); // allows us to use ESJ
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,13 +15,13 @@ const urlDatabase = {
 const generateRandomString = function() {  // used to create random 6 character string. creates random index value and pushes character associated to it to a result
   let i = 0;
   let result = "";
-  let charset = "0123456789abcdefghijklmnopqrstuvwxyz"
+  let charset = "0123456789abcdefghijklmnopqrstuvwxyz";
   do {
     let y = Math.floor(Math.random() * 36);
     result += charset[y];
     i++;
   }
-  while (i < 6) 
+  while (i < 6);
   return result;
 };
 
@@ -39,7 +39,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 }); */
 
-app.get("/urls", (req, res) => { // Line 42-54 are used to set up how the server reacts to get requests. 
+app.get("/urls", (req, res) => { // Line 42-54 are used to set up how the server reacts to get requests.
   const templateVars = { urls: urlDatabase }; //They use ESJ to render their respective esj files using the template variables assigned to them
   res.render("urls_index", templateVars);
 });
@@ -61,7 +61,7 @@ app.get("/u/:shortURL", (req, res) => { // this app.get is responsilbe for makin
 app.post("/urls", (req, res) => { // responds to the post requests made by the form in /urls/new
   const rShortURL = generateRandomString(); // creates a new random short url
   urlDatabase[rShortURL] = req.body.longURL; // updates database
-  res.redirect(302, `/urls/${rShortURL}`) // redirects to the result
+  res.redirect(302, `/urls/${rShortURL}`); // redirects to the result
 });
 
 
