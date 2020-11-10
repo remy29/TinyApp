6a8da9dd4e955cc2c0be849a2c4e3df2f53c3b5c
remy29 <arthur.d.remy@gmail.com>
2020-11-10 02:47:17 +0000

    added various comments for clarity to express_server.js added a .post method

--- express_server.js
+++ express_server.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require("express");  //Lines 1-4 gives express_server.js access to all its required dependencies
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); // allows us to use the body-parser middle-ware to convert request body to readable string
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // allows us to use ESJ 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req, res) => {
+// Lines 14-25 are code used for demonstration of .get functionality
+/* app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
@@ -22,10 +22,10 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+}); */
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+app.get("/urls", (req, res) => { // Line 27-39 are used to set up how the server reacts to get requests. 
+  const templateVars = { urls: urlDatabase }; //They use ESJ to render their respective esj files using the template variables assigned to them
   res.render("urls_index", templateVars);
 });
 
@@ -36,6 +36,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
