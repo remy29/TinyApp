14a117a30198d03ce4ab0b6e35f54ec0ff3b88a3
remy29 <arthur.d.remy@gmail.com>
2020-11-11 22:37:32 +0000

    added registration landing page and get request to match, made various stylistic improvements

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("registration", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"],};
   res.render("urls_show", templateVars);
