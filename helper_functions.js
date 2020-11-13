9fb69d78ea7a227f3df25c0c78f2c9085930551b
remy29 <arthur.d.remy@gmail.com>
2020-11-13 01:32:37 +0000

    refactored helper functions to be modular

--- helper_functions.js
+++ helper_functions.js
@@ -1,4 +1,4 @@
-const generateRandomString = function() {  // used to create random 6 character string. creates random index value and pushes character associated to it to a result
+const generateRandomString = function () {  // used to create random 6 character string. creates random index value and pushes character associated to it to a result
   let i = 0;
   let result = "";
   let charset = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -11,36 +11,39 @@ const generateRandomString = function() {  // used to create random 6 character 
   return result;
 };
 
-const userChecker = function(req) { //checks if user already exists
+const userChecker = function (email, database) { //checks if user already exists
   let user;
-  for (const id in userDB) {
-    if (userDB[id]["email"] === req.body.email) {
-      user = userDB[id];
+  for (const id in database) {
+    if (database[id]["email"] === email) {
+      user = database[id];
     }
   }
   return user;
 };
+userChecker(req.body.email, userDB);
 
-const urlsForUser = function(id) {
+const urlsForUser = function (id, database) {
   const foundURLs = {};
-  for (const shortURLs in urlDatabase) {
-    if (urlDatabase[shortURLs]["userID"] === id) {
-      foundURLs[shortURLs] = urlDatabase[shortURLs]["longURL"];
+  for (const shortURLs in database) {
+    if (database[shortURLs]["userID"] === id) {
+      foundURLs[shortURLs] = database[shortURLs]["longURL"];
     }
   }
   return foundURLs;
 };
+urlsForUser(req.session["user_id"], urlDatabase)
 
-const isLoggedIn = function(req) {
-  const currentUser = req.session["user_id"] ? `${userDB[req.session["user_id"]]["email"]}` : "Unregistered Guest";
+const isLoggedIn = function (id, database) {
+  const currentUser = id ? `${database[id]["email"]}` : "Unregistered Guest";
   return currentUser;
 };
+isLoggedIn(req.session["user_id"], userDB)
 
 module.exports = { generateRandomString, userChecker, urlsForUser, isLoggedIn }
 
 
 
-const { generateRandomString } = require('./helper_functions')
-const { userChecker } = require('./helper_functions')
-const { urlsForUser } = require('./helper_functions')
-const { generateRandomString } = require('./helper_functions')
+const generateRandomString = require('./helper_functions').generateRandomString
+const userChecker = require('./helper_functions').userChecker
+const urlsForUser = require('./helper_functions').urlsForUser
+const isLoggedIn = require('./helper_functions').isLoggedIn
